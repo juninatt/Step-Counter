19b36e25471f321a31bc230214ed7999e471dd59
Olof Kastrup <olofkastrup@gmail.com>
2019-09-23 10:47:56 +0200

    Add Step entity and JPA support for H2 database

---
+++ src/main/java/se/sigma/boostapp/boost_app_java/repository/StepRepository.java
@@ -0,0 +1,8 @@
+package se.sigma.boostapp.boost_app_java.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import se.sigma.boostapp.boost_app_java.step.Step;
+
+public interface StepRepository extends CrudRepository<Step, Long>{
+
+}
--- src/main/java/se/sigma/boostapp/boost_app_java/step/Step.java
+++ src/main/java/se/sigma/boostapp/boost_app_java/step/Step.java
@@ -1,5 +1,69 @@
 package se.sigma.boostapp.boost_app_java.step;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Step {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column
+	private int stepCount;
+
+	@Column
+	private LocalDateTime start;
+
+	@Column
+	private LocalDateTime end;
+
+	public Step() {
+
+	}
+
+	public Step(int stepCount, LocalDateTime start, LocalDateTime end) {
+		this.stepCount = stepCount;
+		this.start = start;
+		this.end = end;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getStepCount() {
+		return stepCount;
+	}
+
+	public void setStepCount(int stepCount) {
+		this.stepCount = stepCount;
+	}
+
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getEnd() {
+		return end;
+	}
+
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
 
 }
