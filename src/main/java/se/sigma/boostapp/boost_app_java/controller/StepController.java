083a7878e13c6021aa083df31ab23883dc915285
Chi Trinh <chikun_88@hotmail.com>
2019-09-23 09:27:08 +0200

    Add package and resource folder

---
+++ src/main/java/se/sigma/boostapp/boost_app_java/controller/StepController.java
@@ -0,0 +1,5 @@
+package se.sigma.boostapp.boost_app_java.controller;
+
+public class StepController {
+
+}
---
+++ src/main/java/se/sigma/boostapp/boost_app_java/step/Step.java
@@ -0,0 +1,5 @@
+package se.sigma.boostapp.boost_app_java.step;
+
+public class Step {
+
+}
