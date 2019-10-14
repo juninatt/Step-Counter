13e924c0f55323366607d05fd62700c800eab294
Chi Trinh <chikun_88@hotmail.com>
2019-10-14 10:47:40 +0200

    Replace xml file to yaml for Liquibase

--- src/main/java/se/sigma/boostapp/boost_app_java/model/StepDTO.java
+++ src/main/java/se/sigma/boostapp/boost_app_java/model/StepDTO.java
@@ -2,12 +2,25 @@ package se.sigma.boostapp.boost_app_java.model;
 
 import java.time.LocalDateTime;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "All details about the step entity. ")
 public class StepDTO {
 	
+	@ApiModelProperty(notes = "step count")
 	private int stepCount;
+	
+	@ApiModelProperty(notes = "Start time")
 	private LocalDateTime startTime;
+	
+	@ApiModelProperty(notes = "End time")
 	private LocalDateTime endTime;
 	
+	
+	public StepDTO() {
+		
+	}
 	public StepDTO(int stepCount, String start, String end) {
 		this.stepCount = stepCount;
 		this.startTime = LocalDateTime.parse(start);
