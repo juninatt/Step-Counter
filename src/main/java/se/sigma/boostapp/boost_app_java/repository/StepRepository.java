eed15301a8fa5b36916a65e2ed6d6f776bcaea38
Olof Kastrup <olofkastrup@gmail.com>
2020-01-30 10:06:41 +0100

    Fix JPQL query from to_char() to cast() to make the query database agnostic. Hopefully

--- src/main/java/se/sigma/boostapp/boost_app_java/repository/StepRepository.java
+++ src/main/java/se/sigma/boostapp/boost_app_java/repository/StepRepository.java
@@ -1,5 +1,6 @@
 package se.sigma.boostapp.boost_app_java.repository;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +16,6 @@ public interface StepRepository extends CrudRepository<Step, Long>{
 	List<Step> findByUserIdAndStartTimeGreaterThanEqualAndEndTimeLessThanEqual(
 			String userId, LocalDateTime startTime, LocalDateTime endTime);
 
-	@Query("SELECT to_char(s.startTime, 'yyyy-mm-dd'), sum(s.stepCount) FROM Step s WHERE s.userId = :userId AND to_char(s.startTime, 'yyyy-mm-dd') >= :startDate AND to_char(s.startTime, 'yyyy-mm-dd') <= :endDate GROUP BY s.userId, to_char(s.startTime, 'yyyy-mm-dd') ORDER BY to_char(s.startTime,'yyyy-mm-dd') ASC")
-	List getStepCount(@Param("userId") String userId, @Param("startDate") String startDate, @Param("endDate") String endDate);
+	@Query("SELECT cast(s.startTime as date), sum(s.stepCount) FROM Step s WHERE s.userId = :userId AND cast(s.startTime as date) >= :startDate AND cast(s.startTime as date) <= :endDate GROUP BY s.userId, cast(s.startTime as date) ORDER BY cast(s.startTime as date) ASC")
+	List getStepCount(@Param("userId") String userId, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
 }
--- src/main/java/se/sigma/boostapp/boost_app_java/service/StepService.java
+++ src/main/java/se/sigma/boostapp/boost_app_java/service/StepService.java
@@ -1,5 +1,6 @@
 package se.sigma.boostapp.boost_app_java.service;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -48,20 +49,28 @@ public class StepService {
 
 // Get step count per day by user ID
 	public List getStepsByUser(String userId, String startDate, String endDate) {
+		Date firstDate = Date.valueOf(startDate);
+		Date lastDate;
 		if (endDate == null || endDate.equals("")) {
-			endDate = LocalDateTime.now().toString();
+			lastDate = Date.valueOf(LocalDateTime.now().toString());
+		} else {
+			lastDate = Date.valueOf(endDate);
 		}
-		return stepRepository.getStepCount(userId, startDate, endDate);
+		return stepRepository.getStepCount(userId, firstDate, lastDate);
 	}
 
 // Get step count per day per multiple users
 	public List getStepsByMultipleUsers(List<String> users, String startDate, String endDate) {
+		Date firstDate = Date.valueOf(startDate);
+		Date lastDate;
 		if (endDate == null || endDate.equals("")) {
-			endDate = LocalDateTime.now().toString();
+			lastDate = Date.valueOf(LocalDateTime.now().toString());
+		} else {
+			lastDate = Date.valueOf(endDate);
 		}
 		List<List> userStepList = new ArrayList<>();
 		for (String s : users) {
-			userStepList.add(stepRepository.getStepCount(s, startDate, endDate));
+			userStepList.add(stepRepository.getStepCount(s, firstDate, lastDate));
 		}
 		return userStepList;
 	}
