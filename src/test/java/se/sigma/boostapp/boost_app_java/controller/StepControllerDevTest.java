2974eb8e5666a1dd4c11bcfd8abffdeb0535c0fd
Johanna <svallingson@gmail.com>
2020-09-02 11:59:21 +0200

    added tests  StepControllerDev

--- src/test/java/se/sigma/boostapp/boost_app_java/controller/StepControllerDevTest.java
+++ src/test/java/se/sigma/boostapp/boost_app_java/controller/StepControllerDevTest.java
@@ -1,35 +1,37 @@
 package se.sigma.boostapp.boost_app_java.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.Before;
-import org.junit.BeforeClass;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.BDDMockito.given;
+import org.mockito.Mockito;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import se.sigma.boostapp.boost_app_java.dto.StepDTO;
 import se.sigma.boostapp.boost_app_java.model.Step;
 
-import se.sigma.boostapp.boost_app_java.security.SecurityConfig;
+
 import se.sigma.boostapp.boost_app_java.service.StepService;
 
 
@@ -41,18 +43,15 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@AutoConfigureMockMvc
+
 @RunWith(SpringRunner.class)
-//@WebMvcTest(StepControllerDev.class)
 public class StepControllerDevTest {
 
     @MockBean
     private StepService service;
-
-    @InjectMocks
-    private StepControllerDev stepControllerDev;
 
     private MockMvc mvc;
 
@@ -60,7 +59,7 @@ public class StepControllerDevTest {
 
       @Before
       public void setUp(){
-          mvc = MockMvcBuilders.standaloneSetup(stepControllerDev).build();
+          mvc = MockMvcBuilders.standaloneSetup(new StepControllerDev(service)).build();
 
       }
 
@@ -68,7 +67,6 @@ public class StepControllerDevTest {
     @Test
     public void shouldReturnLatestEntryWithUserId() throws Exception{
 
-        //StepService service = mock(StepService.class);
 
         Step step = new Step("testId",300,
                 LocalDateTime.parse("2020-01-01T00:00:00"),
@@ -79,30 +77,51 @@ public class StepControllerDevTest {
 
         mvc.perform(get("/steps/latest/{userId}" ,"testId"))
                 .andDo(print())
+                .andExpect(jsonPath("$.userId").value("testId"))
                 .andExpect(status().isOk());
 
     }
 
     @Test
-    public void shouldRegisterStepCountSuccessfully() throws Exception{
+    public void shouldReturnBadRequestWhenStepCount0() throws Exception{
+        objectMapper.registerModule(new JavaTimeModule());
 
+        StepDTO stepDTO = new StepDTO(0,
+                LocalDateTime.parse("2020-01-01T00:00:00"),
+                LocalDateTime.parse("2020-01-01T01:00:00"),
+                LocalDateTime.parse("2020-01-01T02:00:00"));
+
+        mvc.perform(MockMvcRequestBuilders.post("/steps/{userId}", "testId")
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8")
+                .content(objectMapper.writeValueAsString(stepDTO)).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    public void registerStepsTest() throws Exception {
+        objectMapper.registerModule(new JavaTimeModule());
+        Step mockStep = new Step("testId",
+                100,
+                LocalDateTime.parse("2020-01-01T00:00:00"),
+                LocalDateTime.parse("2020-01-01T00:00:00"),
+                LocalDateTime.parse("2020-01-01T00:00:00"));
         StepDTO stepDTO = new StepDTO(300,
                 LocalDateTime.parse("2020-01-01T00:00:00"),
                 LocalDateTime.parse("2020-01-01T01:00:00"),
                 LocalDateTime.parse("2020-01-01T02:00:00"));
+        when(service.registerSteps(Mockito.anyString(),
+                Mockito.any(StepDTO.class))).thenReturn(Optional.of(mockStep));
 
-        Step step = new Step("testId",300,
-                LocalDateTime.parse("2020-01-01T00:00:00"),
-                LocalDateTime.parse("2020-01-01T01:00:00"),
-                LocalDateTime.parse("2020-01-01T02:00:00"));
-
-        when(service.registerSteps("testId", stepDTO)).thenReturn(Optional.of(step));
-
-        mvc.perform(post("/steps/{userId}", "testId")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(stepDTO)))
-                .andDo(print())
-                .andExpect(status().isOk());
-
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/steps/{userId}", "testId")
+                .accept(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(stepDTO))
+                .contentType(MediaType.APPLICATION_JSON);
+        MvcResult result = mvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertTrue(response.getContentAsString().contains("testId"));
     }
 }
