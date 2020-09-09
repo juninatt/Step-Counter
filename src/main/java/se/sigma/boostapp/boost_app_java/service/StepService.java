0ba966f9652656e2a37d4fc9e216a8202a7c43d1
Johanna <svallingson@gmail.com>
2020-09-09 15:05:00 +0200

    fixed some mistakes

--- src/main/java/se/sigma/boostapp/boost_app_java/service/StepService.java
+++ src/main/java/se/sigma/boostapp/boost_app_java/service/StepService.java
@@ -41,7 +41,9 @@ public class StepService {
         Step latest;
         //if new user, add all to db
         if (!stepRepository.findFirstByUserIdOrderByEndTimeDesc(userId).isPresent()) {
-            for (StepDTO s : stepDtoList) {
+            Map<Integer, List<StepDTO>> groupedByDayOfYearMap =  groupObjectsInListsByEndDate(stepDtoList);
+            stepDtoList = mergeStepDtoObjectsWithSameDate(groupedByDayOfYearMap);
+        	for (StepDTO s : stepDtoList) {
                 stepList.add(addStepToDB(userId, s));
             }
         } else {
@@ -49,11 +51,11 @@ public class StepService {
             latest = stepRepository.findFirstByUserIdOrderByEndTimeDesc(userId).get();
             //filter out any object in list that is before latest entry
             // TODO: 2020-09-08 communicate conflict with response?
+			//objects that conflicts with latest entry in BD will be discarded
             stepDtoList = stepDtoList.stream().filter(stepDTO -> stepDTO.getEndTime().isAfter(latest.getEnd())).collect(Collectors.toList());
 
             //Group objects in lists by endDate
-            Map<Integer, List<StepDTO>> groupedByDayOfYearMap = stepDtoList.stream()
-                    .collect(Collectors.groupingBy(sDto -> sDto.getEndTime().getDayOfYear()));
+            Map<Integer, List<StepDTO>> groupedByDayOfYearMap =  groupObjectsInListsByEndDate(stepDtoList);
 
             if (!stepDtoList.isEmpty()) {
                 //Return list of merged objects with the same endDate
@@ -79,6 +81,12 @@ public class StepService {
         }
         return stepList;
     }
+
+    public Map<Integer, List<StepDTO>> groupObjectsInListsByEndDate(List<StepDTO> list) {
+        return list.stream()
+                .collect(Collectors.groupingBy(sDto -> sDto.getEndTime().getDayOfYear()));
+    }
+
 
     public Step addStepToDB(String id, StepDTO s) {
         return stepRepository.save(new Step(id, s.getStepCount(), s.getStartTime(), s.getEndTime(), s.getUploadedTime()));
@@ -115,119 +123,117 @@ public class StepService {
 
     }
 
-	public Optional<Step> registerSteps(String userId, StepDTO stepDto) {
-		List<String> usersId = stepRepository.getAllUsers();
+    public Optional<Step> registerSteps(String userId, StepDTO stepDto) {
+        List<String> usersId = stepRepository.getAllUsers();
 
-		//koden skriver först alla rader (för olika dagar) för en användare och
-		//den alla rader för andra användare ....
-		if (usersId.contains(userId)) {
-			Step existingStep = stepRepository.findFirstByUserIdOrderByEndTimeDesc(userId).get();
-			
-			if(existingStep.getEnd().getDayOfYear() == stepDto.getEndTime().getDayOfYear()
-				&& existingStep.getEnd().isBefore(stepDto.getEndTime())) {
-							existingStep.setStepCount(existingStep.getStepCount() + stepDto.getStepCount());
-							existingStep.setEnd(stepDto.getEndTime());
-							existingStep.setStart(stepDto.getStartTime());
-							existingStep.setUploadedTime(stepDto.getUploadedTime());
-				return Optional.of(stepRepository.save(existingStep));
-				}
-			// ta hand bara om end tid
-			else if(existingStep.getEnd().getDayOfYear() == stepDto.getEndTime().getDayOfYear()
-					&& (existingStep.getEnd().equals(stepDto.getEndTime())
-					|| existingStep.getEnd().isAfter(stepDto.getEndTime()))){
-				//tänka att skriva kod för meddelande till användare
-				return Optional.empty();				
-			}
-			else if(existingStep.getEnd().getDayOfYear() != stepDto.getEndTime().getDayOfYear())
-							existingStep.setStepCount(existingStep.getStepCount() + stepDto.getStepCount());
-				return Optional.of(stepRepository.save(new Step(userId, stepDto.getStepCount(), stepDto.getStartTime(), stepDto.getEndTime(), stepDto.getUploadedTime())));
-			
-		}
-		else 
-			return Optional.of(stepRepository.save(new Step(userId, stepDto.getStepCount(), stepDto.getStartTime(), stepDto.getEndTime(), stepDto.getUploadedTime())));
-	}
+        //koden skriver först alla rader (för olika dagar) för en användare och
+        //den alla rader för andra användare ....
+        if (usersId.contains(userId)) {
+            Step existingStep = stepRepository.findFirstByUserIdOrderByEndTimeDesc(userId).get();
+
+            if (existingStep.getEnd().getDayOfYear() == stepDto.getEndTime().getDayOfYear()
+                    && existingStep.getEnd().isBefore(stepDto.getEndTime())) {
+                existingStep.setStepCount(existingStep.getStepCount() + stepDto.getStepCount());
+                existingStep.setEnd(stepDto.getEndTime());
+                existingStep.setStart(stepDto.getStartTime());
+                existingStep.setUploadedTime(stepDto.getUploadedTime());
+                return Optional.of(stepRepository.save(existingStep));
+            }
+            // ta hand bara om end tid
+            else if (existingStep.getEnd().getDayOfYear() == stepDto.getEndTime().getDayOfYear()
+                    && (existingStep.getEnd().equals(stepDto.getEndTime())
+                    || existingStep.getEnd().isAfter(stepDto.getEndTime()))) {
+                //tänka att skriva kod för meddelande till användare
+                return Optional.empty();
+            } else if (existingStep.getEnd().getDayOfYear() != stepDto.getEndTime().getDayOfYear())
+                existingStep.setStepCount(existingStep.getStepCount() + stepDto.getStepCount());
+            return Optional.of(stepRepository.save(new Step(userId, stepDto.getStepCount(), stepDto.getStartTime(), stepDto.getEndTime(), stepDto.getUploadedTime())));
+
+        } else
+            return Optional.of(stepRepository.save(new Step(userId, stepDto.getStepCount(), stepDto.getStartTime(), stepDto.getEndTime(), stepDto.getUploadedTime())));
+    }
 
 
-//	Get latest step entity by user
-	// //2020-09-03 denna metoden skickar en summa av steg per dag per användare
-	// summan ändrar varje 10-minuter (om man gör nya steg) pga metoden registerSteps som fyller databasen varje 10 minuter
-	public Optional<Step> getLatestStep(String userId) {
-		return stepRepository.findFirstByUserIdOrderByEndTimeDesc(userId);
-	}
+    //	Get latest step entity by user
+    // //2020-09-03 denna metoden skickar en summa av steg per dag per användare
+    // summan ändrar varje 10-minuter (om man gör nya steg) pga metoden registerSteps som fyller databasen varje 10 minuter
+    public Optional<Step> getLatestStep(String userId) {
+        return stepRepository.findFirstByUserIdOrderByEndTimeDesc(userId);
+    }
 
-//	Helper method. Get total step count from a list of Steps
-	public int getStepCount(List<Step> steps) {
-		int total = 0;
-		for (Step step : steps) {
-			total += step.getStepCount();
-		}
-		return total;
-	}
+    //	Helper method. Get total step count from a list of Steps
+    public int getStepCount(List<Step> steps) {
+        int total = 0;
+        for (Step step : steps) {
+            total += step.getStepCount();
+        }
+        return total;
+    }
 
-//	Get sum of step count by userId, start date and end Date.
-	public int getStepSumByUser(String userId, String startDate, String endDate) {
-		LocalDateTime end;
-		if (endDate == null || endDate.equals("")) {
-			end = LocalDateTime.now();
-		} else {
-			end = LocalDate.parse(endDate).atTime(23, 59, 59);
-		}
-		List<Step> allSteps = stepRepository.findByUserIdAndStartTimeGreaterThanEqualAndEndTimeLessThanEqual(userId,
-				LocalDate.parse(startDate).atStartOfDay(), end);
-		return getStepCount(allSteps);
-	}
+    //	Get sum of step count by userId, start date and end Date.
+    public int getStepSumByUser(String userId, String startDate, String endDate) {
+        LocalDateTime end;
+        if (endDate == null || endDate.equals("")) {
+            end = LocalDateTime.now();
+        } else {
+            end = LocalDate.parse(endDate).atTime(23, 59, 59);
+        }
+        List<Step> allSteps = stepRepository.findByUserIdAndStartTimeGreaterThanEqualAndEndTimeLessThanEqual(userId,
+                LocalDate.parse(startDate).atStartOfDay(), end);
+        return getStepCount(allSteps);
+    }
 
-// Get step count per day by user ID
-	// getLatestStep nu gör denna räkning
-	public List<StepDateDTO> getStepsByUser(String userId, String startDate, String endDate) {
-		Date firstDate = Date.valueOf(startDate);
-		Date lastDate;
-		if (endDate == null || endDate.equals("")) {
-			lastDate = Date.valueOf(LocalDate.now());
-		} else {
-			lastDate = Date.valueOf(endDate);
-		}
-		return stepRepository.getStepCount(userId, firstDate, lastDate);
-	}
+    // Get step count per day by user ID
+    // getLatestStep nu gör denna räkning
+    public List<StepDateDTO> getStepsByUser(String userId, String startDate, String endDate) {
+        Date firstDate = Date.valueOf(startDate);
+        Date lastDate;
+        if (endDate == null || endDate.equals("")) {
+            lastDate = Date.valueOf(LocalDate.now());
+        } else {
+            lastDate = Date.valueOf(endDate);
+        }
+        return stepRepository.getStepCount(userId, firstDate, lastDate);
+    }
 
-// Get step count per day per multiple users
-	public Optional<List<BulkUsersStepsDTO>> getStepsByMultipleUsers(List<String> users, String startDate, String endDate) {
+    // Get step count per day per multiple users
+    public Optional<List<BulkUsersStepsDTO>> getStepsByMultipleUsers(List<String> users, String startDate, String endDate) {
 
-		// String startDate → java.sql.Date firstDate
-		Date firstDate = Date.valueOf(startDate);
-		Date lastDate;
-		if (endDate == null || endDate.equals("")) {
-			lastDate = Date.valueOf(LocalDate.now());
-		} else {
-			lastDate = Date.valueOf(endDate);
-		}
-		List<String> allUsers = stepRepository.getAllUsers();
+        // String startDate → java.sql.Date firstDate
+        Date firstDate = Date.valueOf(startDate);
+        Date lastDate;
+        if (endDate == null || endDate.equals("")) {
+            lastDate = Date.valueOf(LocalDate.now());
+        } else {
+            lastDate = Date.valueOf(endDate);
+        }
+        List<String> allUsers = stepRepository.getAllUsers();
 
-		BulkUsersStepsDTO bulkUsersStepsDTO;
-		List<BulkUsersStepsDTO> bulkUsersStepsDTOList = new ArrayList<>();
+        BulkUsersStepsDTO bulkUsersStepsDTO;
+        List<BulkUsersStepsDTO> bulkUsersStepsDTOList = new ArrayList<>();
 
-		for (String s : users) {
-			if (!allUsers.contains(s)) continue;
-			bulkUsersStepsDTO = new BulkUsersStepsDTO(s, stepRepository.getStepCount(s, firstDate, lastDate));
-			bulkUsersStepsDTOList.add(bulkUsersStepsDTO);
-		}
-		return bulkUsersStepsDTOList.isEmpty() ? Optional.empty() : Optional.of(bulkUsersStepsDTOList);
-	}
+        for (String s : users) {
+            if (!allUsers.contains(s)) continue;
+            bulkUsersStepsDTO = new BulkUsersStepsDTO(s, stepRepository.getStepCount(s, firstDate, lastDate));
+            bulkUsersStepsDTOList.add(bulkUsersStepsDTO);
+        }
+        return bulkUsersStepsDTOList.isEmpty() ? Optional.empty() : Optional.of(bulkUsersStepsDTOList);
+    }
 
-// Translate steps to star points for a list of users
-	public List<BulkUserStarPointsDTO> getStarPointsByMultipleUsers(RequestStarPointsDTO requestStarPointsDTO) {
+    // Translate steps to star points for a list of users
+    public List<BulkUserStarPointsDTO> getStarPointsByMultipleUsers(RequestStarPointsDTO requestStarPointsDTO) {
 
-		if (requestStarPointsDTO.getUsers() == null) requestStarPointsDTO.setUsers(stepRepository.getAllUsers());
-		return requestStarPointsDTO.getUsers().stream()
-				.filter(user -> (stepRepository.getStepCountSum(user, requestStarPointsDTO.getStartTime(), requestStarPointsDTO.getEndTime())).isPresent())
-				.map(user -> new BulkUserStarPointsDTO(user, new StarPointDateDTO(
-						"Steps",
-						"Walking",
-						requestStarPointsDTO.getStartTime().toString(),
-						requestStarPointsDTO.getEndTime().toString(),
-						(int) Math.ceil(
-								(stepRepository.getStepCountSum(user, requestStarPointsDTO.getStartTime(), requestStarPointsDTO.getEndTime())).get()
-										* starPointFactor)
-				))).collect(Collectors.toList());
-	}
+        if (requestStarPointsDTO.getUsers() == null) requestStarPointsDTO.setUsers(stepRepository.getAllUsers());
+        return requestStarPointsDTO.getUsers().stream()
+                .filter(user -> (stepRepository.getStepCountSum(user, requestStarPointsDTO.getStartTime(), requestStarPointsDTO.getEndTime())).isPresent())
+                .map(user -> new BulkUserStarPointsDTO(user, new StarPointDateDTO(
+                        "Steps",
+                        "Walking",
+                        requestStarPointsDTO.getStartTime().toString(),
+                        requestStarPointsDTO.getEndTime().toString(),
+                        (int) Math.ceil(
+                                (stepRepository.getStepCountSum(user, requestStarPointsDTO.getStartTime(), requestStarPointsDTO.getEndTime())).get()
+                                        * starPointFactor)
+                ))).collect(Collectors.toList());
+    }
 }
