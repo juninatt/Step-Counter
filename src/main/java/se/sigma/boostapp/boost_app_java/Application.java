252e4977568c42df4efcee9136bd9e6f3cf85c94
Danijela <danijela.blace@sigma.se>
2020-09-28 13:36:40 +0200

    javadoc

--- src/main/java/se/sigma/boostapp/boost_app_java/Application.java
+++ src/main/java/se/sigma/boostapp/boost_app_java/Application.java
@@ -15,6 +15,30 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 
+
+/*
+ * javadoc syntaks
+ * 
+ * @author (classes and interfaces only, required)
+@version (classes and interfaces only, required. See footnote 1)
+@param (methods and constructors only)
+@return (methods only)
+@exception (@throws is a synonym added in Javadoc 1.2)
+@see
+@since
+@serial (or @serialField or @serialData)
+@deprecated (see How and When To Deprecate APIs)
+ * 
+ * 
+ */
+/**
+ * @author SigmaIT
+ * 
+ * <br> Start application and start scheduling( deleting table step on the Monday 00:00:01 )
+ */
+
+
+
 @SpringBootApplication
 @Configuration
 @EnableScheduling
