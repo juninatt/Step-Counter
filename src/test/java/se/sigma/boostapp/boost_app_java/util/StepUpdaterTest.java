4fe35ef6afc2044c37fc9c96845f39634ce14318
Petter Bergström <Petter.Bergstrom@nexergroup.com>
2023-01-24 14:55:41 +0000

    Merged PR 1544: Change method name in StepUpdaterTest.java
    - change method name to correctly correspond with tested method

--- src/test/java/se/sigma/boostapp/boost_app_java/util/StepUpdaterTest.java
+++ src/test/java/se/sigma/boostapp/boost_app_java/util/StepUpdaterTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import se.sigma.boostapp.boost_app_java.dto.stepdto.StepDTO;
-import se.sigma.boostapp.boost_app_java.model.MonthStep;
 import se.sigma.boostapp.boost_app_java.model.Step;
-import se.sigma.boostapp.boost_app_java.model.WeekStep;
 
 import java.time.LocalDateTime;
 
@@ -29,14 +27,8 @@ public class StepUpdaterTest {
     @BeforeEach
     public void setUp() {
         stepUpdater = StepUpdater.getInstance();
-
         testDto = new StepDTO(69, START_TIME, END_TIME, UPLOADED_TIME);
         testStep = new Step("testStep", 100, END_TIME);
-        int YEAR = 2023;
-        int MONTH = 1;
-        MonthStep testMonthStep = new MonthStep("testStep", MONTH, YEAR, 100);
-        int WEEK = 1;
-        WeekStep testWeekStep = new WeekStep("testStep", WEEK, YEAR, 100);
     }
     @Nested
     @DisplayName("updateCurrentStep ")
@@ -45,7 +37,7 @@ public class StepUpdaterTest {
         @Test
         @DisplayName("should return object of Step class")
         public void testUpdateCurrentStep_ReturnsObjectOfStepClass() {
-            var updatedStep = stepUpdater.updateStep(testStep, testDto);
+            var updatedStep = stepUpdater.update(testStep, testDto);
 
             MatcherAssert.assertThat(updatedStep, instanceOf(Step.class));
         }
@@ -53,7 +45,7 @@ public class StepUpdaterTest {
         @Test
         @DisplayName("should update stepCount of Step object")
         public void testUpdateCurrentStep_UpdatesStepCount() {
-            var upDatedStep = stepUpdater.updateStep(testStep, testDto);
+            var upDatedStep = stepUpdater.update(testStep, testDto);
 
             var expected = 169;
 
@@ -63,14 +55,14 @@ public class StepUpdaterTest {
         @Test
         @DisplayName("should update end time of Step object")
         public void testUpdateCurrentStep_UpdatesEndTime() {
-            var upDatedStep = stepUpdater.updateStep(testStep, testDto);
+            var upDatedStep = stepUpdater.update(testStep, testDto);
 
             assertEquals(upDatedStep.getEndTime(), END_TIME);
         }
         @Test
         @DisplayName("should update upload time of Step object")
         public void testUpdateCurrentStep_UpdatesUploadTime() {
-            var upDatedStep = stepUpdater.updateStep(testStep, testDto);
+            var upDatedStep = stepUpdater.update(testStep, testDto);
 
             assertEquals(upDatedStep.getUploadedTime(), UPLOADED_TIME);
         }
