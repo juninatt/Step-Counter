99584af05e20b3a8f118435e70a9a91f702abe12
Petter Bergström <Petter.Bergstrom@nexergroup.com>
2023-02-02 09:12:44 +0000

    Merged PR 1557: Add javaDoc to OpenApiConfiguration.java
    Add javaDoc to OpenApiConfiguration.java
    
    - add javaDoc for class and methods
    
    #2762
    #2764
    
    Related work items: #2762, #2764

--- src/main/java/se/sigma/boostapp/boost_app_java/config/OpenApiConfiguration.java
+++ src/main/java/se/sigma/boostapp/boost_app_java/config/OpenApiConfiguration.java
@@ -9,8 +9,23 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for BoostApp Step-Api.
+ * This class creates a {@link OpenAPI} object
+ * and sets the security requirements, components, servers and info for the BoostApp Step-Api.
+ * The security requirement added is the "Bearer Authorization" using JWT.
+ * The API info includes the title, description, and version.
+ *
+ */
 @Configuration
 public class OpenApiConfiguration {
+
+    /**
+     * Creates a custom {@link OpenAPI} object for BoostApp Step-Api.
+     *
+     * @return {@link OpenAPI} object with security requirements, components,
+     *  servers and API info set for BoostApp Step-Api.
+     */
     @Bean
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "Bearer Authorization";
@@ -31,6 +46,11 @@ public class OpenApiConfiguration {
                 .info(apiInfo());
     }
 
+    /**
+     * Creates an {@link Info} object for BoostApp Step-Api.
+     *
+     * @return {@link Info} object with title, description, and version set for BoostApp Step-Api.
+     */
     private Info apiInfo() {
         return new Info()
                 .title("Step API Documentation")
