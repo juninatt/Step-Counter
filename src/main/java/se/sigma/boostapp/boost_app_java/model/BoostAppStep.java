5d43bb86f67b27878a1dfe26e5dda14481e55184
Petter Bergström <Petter.Bergstrom@nexergroup.com>
2023-02-03 14:42:41 +0000

    Merged PR 1564: Add javaDoc to BoostAppStep.java
    Add javaDoc to BoostAppStep.java
    
    - add javaDoc to interface and its methods
    
    #2774
    
    Related work items: #2774

--- src/main/java/se/sigma/boostapp/boost_app_java/model/BoostAppStep.java
+++ src/main/java/se/sigma/boostapp/boost_app_java/model/BoostAppStep.java
@@ -1,9 +1,40 @@
 package se.sigma.boostapp.boost_app_java.model;
 
+/**
+ * This interface defines the methods to set and get the userId and stepCount fields of the implementing classes
+ *
+ * @see Step
+ * @see WeekStep
+ * @see MonthStep
+ */
 public interface BoostAppStep {
+
+    /**
+     * Set the value for userId from the BoostAppStep.
+     *
+     * @param userId the value to set the userId to
+     */
     void setUserId(String userId);
+
+    /**
+     * Get the value for userId from the BoostAppStep.
+     *
+     * @return the userId value
+     */
     String getUserId();
+
+    /**
+     * Set the value for stepCount from the BoostAppStep.
+     *
+     * @param stepCount the value to set the stepCount to
+     */
     void setStepCount(int stepCount);
+
+    /**
+     * Get the value for stepCount from the BoostAppStep.
+     *
+     * @return the stepCount value
+     */
     int getStepCount();
 
 }
