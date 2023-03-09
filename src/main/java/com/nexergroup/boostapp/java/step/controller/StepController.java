e695c1bc2a1dc3054377e24b8088ee3936303f41
Petter Bergström <Petter.Bergstrom@nexergroup.com>
2023-03-09 09:32:35 +0000

    Merged PR 1659: Add 'getStepCountByDayForUserAndDate' to production
    Add 'getStepCountByDayForUserAndDate' to production
    
    Related work items: #2900

--- src/main/java/com/nexergroup/boostapp/java/step/controller/StepController.java
+++ src/main/java/com/nexergroup/boostapp/java/step/controller/StepController.java
@@ -4,6 +4,7 @@ import com.nexergroup.boostapp.java.step.controller.apiresponse.GroupedApiRespon
 import com.nexergroup.boostapp.java.step.dto.stepdto.BulkStepDateDTO;
 import com.nexergroup.boostapp.java.step.dto.stepdto.StepDTO;
 import com.nexergroup.boostapp.java.step.dto.stepdto.StepDateDTO;
+import com.nexergroup.boostapp.java.step.dto.stepdto.WeekStepDTO;
 import com.nexergroup.boostapp.java.step.exception.NotFoundException;
 import com.nexergroup.boostapp.java.step.model.Step;
 import com.nexergroup.boostapp.java.step.service.StepService;
@@ -175,5 +176,12 @@ public class StepController {
         return stepService.createBulkStepDateDtoForUserForCurrentWeek(JwtValidator.getUserId(jwt))
                 .map(ResponseEntity::ok)
                 .orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
+    }
+
+    @Operation(summary = "Get stepCount per day for current week for a specific user")
+    @GroupedApiResponse
+    @GetMapping(value = "/stepcount/{userId}/currentweekdaily")
+    public WeekStepDTO getStepCountByDayForUserAndDate(final @AuthenticationPrincipal @Parameter(hidden = true) Jwt jwt) {
+        return stepService.getStepsPerDayForWeek(JwtValidator.getUserId(jwt));
     }
 }
