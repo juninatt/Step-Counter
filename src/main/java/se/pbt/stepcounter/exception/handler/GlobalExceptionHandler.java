8516d8dbb626121874fe8afcced1ddd487855f15
Petter B <petter.bergstrom@protonmail.com>
2023-06-10 16:09:41 +0200

    Add exception to GlobalExceptionHandler.java

--- src/main/java/se/pbt/stepcounter/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/se/pbt/stepcounter/exception/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import se.pbt.stepcounter.exception.InvalidStepDataException;
 import se.pbt.stepcounter.exception.InvalidUserIdException;
 import se.pbt.stepcounter.exception.NotFoundException;
 
@@ -25,6 +26,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 				exception.getMessage(),
 				exception.getCause()
 		);
+	}
+
+	@ExceptionHandler(InvalidStepDataException.class)
+	public ResponseEntity<ErrorResponse<?>> handleValidationFailedException(InvalidUserIdException exception) {
+		var errorResponse = new ErrorResponse<>();
+		errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+		errorResponse.setMessage(exception.getMessage());
+		errorResponse.setTimeStamp(System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
 	}
 
 	@ExceptionHandler(NotFoundException.class)
@@ -58,7 +68,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 
-	private <T> ResponseEntity<ErrorResponse<?>> createErrorResponse(HttpStatus status, String message, Throwable cause) {
+	private ResponseEntity<ErrorResponse<?>> createErrorResponse(HttpStatus status, String message, Throwable cause) {
 		var errorResponse = new ErrorResponse<>();
 		errorResponse.setStatus(status.value());
 		errorResponse.setMessage(message);
