b2d10a05074d1cc57f0fe5433a9e9be73c7fa30a
Petter B <petter.bergstrom@protonmail.com>
2023-06-10 15:17:54 +0200

    Refactor GlobalExceptionHandler.java
    - add exception handling
    - extract logic to private method

--- src/main/java/se/pbt/stepcounter/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/se/pbt/stepcounter/exception/handler/GlobalExceptionHandler.java
@@ -4,9 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import se.pbt.stepcounter.exception.NotFoundException;
 import se.pbt.stepcounter.exception.InvalidUserIdException;
+import se.pbt.stepcounter.exception.NotFoundException;
 
 /**
  *
@@ -17,29 +18,53 @@ import se.pbt.stepcounter.exception.InvalidUserIdException;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<ErrorResponse<?>> handleIllegalArgumentException(IllegalArgumentException illegalArgumentException) {
-		var error = new ErrorResponse<>();
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setMessage(illegalArgumentException.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<ErrorResponse<?>> handleIllegalArgumentException(IllegalArgumentException exception) {
+		return createErrorResponse(
+				HttpStatus.BAD_REQUEST,
+				exception.getMessage(),
+				exception.getCause()
+		);
 	}
 
 	@ExceptionHandler(NotFoundException.class)
-	public ResponseEntity<ErrorResponse<?>> handleNotFoundException(NotFoundException notFoundException) {
-		var errorResponse = new ErrorResponse<>();
-		errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-		errorResponse.setMessage(notFoundException.getMessage());
-		errorResponse.setTimeStamp(System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<ErrorResponse<?>> handleNotFoundException(NotFoundException exception) {
+		return createErrorResponse(
+				HttpStatus.NOT_FOUND,
+				exception.getMessage(),
+				exception
+		);
 	}
 
 	@ExceptionHandler(InvalidUserIdException.class)
-	public ResponseEntity<ErrorResponse<?>> handleUserIdValueException(InvalidUserIdException invalidUserIdException) {
-		var errorResponse = new ErrorResponse<>();
-		errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-		errorResponse.setMessage(invalidUserIdException.getMessage());
-		errorResponse.setTimeStamp(System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<ErrorResponse<?>> handleUserIdValueException(InvalidUserIdException exception) {
+		return createErrorResponse(
+				HttpStatus.BAD_REQUEST,
+				exception.getMessage(),
+				exception
+		);
 	}
+
+	@ExceptionHandler(NullPointerException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public ResponseEntity<ErrorResponse<?>> handleNullPointerException(NullPointerException exception) {
+		return createErrorResponse(
+				HttpStatus.INTERNAL_SERVER_ERROR,
+				exception.getMessage(),
+				exception
+		);
+	}
+
+
+	private <T> ResponseEntity<ErrorResponse<?>> createErrorResponse(HttpStatus status, String message, Throwable cause) {
+		var errorResponse = new ErrorResponse<>();
+		errorResponse.setStatus(status.value());
+		errorResponse.setMessage(message);
+		errorResponse.setTimeStamp(System.currentTimeMillis());
+		errorResponse.setDetails(cause);
+		return ResponseEntity.status(status).body(errorResponse);
+	}
+
 }
