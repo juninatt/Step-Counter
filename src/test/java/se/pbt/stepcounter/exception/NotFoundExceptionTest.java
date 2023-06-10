8ae8c91a6f496b0e9c9940097edd5bc9babc94b8
Petter B <petter.bergstrom@protonmail.com>
2023-06-10 14:26:44 +0200

    Add test to NotFoundExceptionTest.java

--- src/test/java/se/pbt/stepcounter/exception/NotFoundExceptionTest.java
+++ src/test/java/se/pbt/stepcounter/exception/NotFoundExceptionTest.java
@@ -4,13 +4,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("NotFoundException Unit Tests")
+@DisplayName("NotFoundException:")
 public class NotFoundExceptionTest {
 
     @Test
-    @DisplayName("Test NotFoundException Constructor")
-    public void testNotFoundExceptionConstructor() {
+    @DisplayName("Test NotFoundException Constructor with Error Message")
+    public void testConstructorWithErrorMessage() {
+        String errorMessage = "Error Message";
+        NotFoundException exception = new NotFoundException(errorMessage);
+        Assertions.assertEquals(errorMessage, exception.getMessage(), "Error message is not correct");
+    }
+
+    @Test
+    @DisplayName("Test NotFoundException Default Constructor")
+    public void testDefaultConstructor() {
         NotFoundException exception = new NotFoundException();
-        Assertions.assertEquals("Not found", exception.getMessage(), "Error message is not correct");
+        String defaultErrorMessage = "Not found.";
+        Assertions.assertEquals(defaultErrorMessage, exception.getMessage(), "Error message is not correct");
     }
 }
